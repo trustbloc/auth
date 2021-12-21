2b268bc323e4939da45a58f458a64a55d5ed1f9c
talwinder50 <talwinderkaur50@gmail.com>
2021-12-21 08:05:04 -0500

    fix: Update signupText and signinText to logos
    closes #141
    
    Signed-off-by: talwinder50 <talwinderkaur50@gmail.com>

--- pkg/restapi/operation/models.go
+++ pkg/restapi/operation/models.go
@@ -39,9 +39,9 @@ type authProviders struct {
 }
 
 type authProvider struct {
-	ID         string `json:"id"`
-	Name       string `json:"name"`
-	LogoURL    string `json:"logoURL"`
-	SignUpText string `json:"signUpText"`
-	SignInText string `json:"signInText"`
+	ID            string `json:"id"`
+	Name          string `json:"name"`
+	SignUpLogoURL string `json:"signUpLogoUrl"`
+	SignInLogoURL string `json:"signInLogoUrl"`
+	Order         int    `json:"order"`
 }
